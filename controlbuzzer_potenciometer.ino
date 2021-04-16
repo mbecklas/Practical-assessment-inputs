3cc560472942496db10f5fd3392c7400a639c5cc
mbecklas <75644072+mbecklas@users.noreply.github.com>
2021-04-16 17:54:03 -0300

    final version
    its all working, the buzzer, LED and the potentiometer

--- controlbuzzer_potenciometer.ino
+++ controlbuzzer_potenciometer.ino
@@ -4,18 +4,25 @@ void setup(){
   pinMode(8, OUTPUT);
   Serial.begin(9600);
 
+
 }
 void loop()
 {
-  Serial.println(analogRead(A0));
+  int var = analogRead(A0);
+  int y = map(var,0,1023,10000,1000);
 
-  if(analogRead(A0) > 1000){
+  Serial.println(y);
+  Serial.println(var);
+
+  if(var> 1000){
     digitalWrite(8 ,HIGH);
+    noTone(2);
   }else{
-    digitalWrite(8, LOW);
+    digitalWrite(8, LOW); 
+    tone(2,y);
   }
 
 
-
+ 
 
 }
